c62bf0163f734e06a1b12d2907ec2a91a9f5162e
Sam Migachev <sammigachev@gmail.com>
2024-02-06 20:18:33 +0300

    edit message

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args){
-        System.out.println("Открытие магазина ожидается через 3 дня");
+        System.out.println("Открытие магазина ожидается через 1 дня");
     }
 }
